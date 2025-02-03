31be43ed518c9f2a23f95c6089affb947262e4bd
pRottinghuis <70864925+pRottinghuis@users.noreply.github.com>
2025-02-02 17:04:18 -0800

    feat: Init process_data.py Script
    This script is designed to go through the files in the data directory and build them into a 1 compendium. preprocess function removed from download_data.py script.

--- scripts/download_data.py
+++ scripts/download_data.py
@@ -10,10 +10,6 @@ def ensure_dirs():
     """Ensure required directories exist before writing files."""
     os.makedirs(RAW_PATH, exist_ok=True)  # Creates if not exists
 
-def preprocess():
-    """Preprocess downloaded data."""
-
-
 if __name__ == "__main__":
     ensure_dirs()
     # TODO: Use requests library to download data here.
