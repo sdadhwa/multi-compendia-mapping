c4909b00c8157388090cf4a882dedb49c63ff33f
pRottinghuis <70864925+pRottinghuis@users.noreply.github.com>
2025-02-10 11:09:30 -0800

    fix: Pyplot Import
    Adjusted formatting on pyplot alias.

--- scripts/UMAP.py
+++ scripts/UMAP.py
@@ -7,7 +7,7 @@ import numpy as np
 from sklearn.datasets import load_digits
 from sklearn.model_selection import train_test_split
 from sklearn.preprocessing import StandardScaler
-from matplotlib.pyplot import plt
+import matplotlib.pyplot as plt
 import seaborn as sns
 import pandas as pd
 import umap
