50cac3f43ba74379de2b2efa68843b9dd40a349f
pRottinghuis <70864925+pRottinghuis@users.noreply.github.com>
2025-02-12 08:49:11 -0800

    refactor: Expression data preprocessing params default to None
    Set the default of the variance threshold and minimum expression params in process_expression_compendium to none. If they are not explicitly passed in the caller then skip those operations.

--- src/preprocessing.py
+++ src/preprocessing.py
@@ -1,18 +1,17 @@
 import pandas as pd
 import numpy as np
 
-def process_expression_compendium(expression_dict, variance_threshold=20, minimum_expression=0):
+def process_expression_compendium(expression_dict, variance_threshold=None, minimum_expression=None):
     """
-    Build a single data frame out of multiple gene expression data frames. Then remove genes with the lowest variance
-    and expressions. Finally, return the data frame.
+    Build a single data frame out of multiple gene expression data frames. If specified, remove genes with low variance
+    and/or low expression. When trying to do both, minimum_expression is applied first and then variance_threshold.
 
     Args:
         expression_dict (dict): Dictionary where keys are compendium names and values are numpy dataframes containing
-        gene expression data. Each column is a gene and each row is a sample.
-
-
-        variance_threshold (int): What percentile of low variance genes to remove.
-        minimum_expression (float): The threshold for minimum expression. Units: mean log2(TPM+1)
+            gene expression data. Each column is a gene and each row is a sample.
+        variance_threshold (int): What percentile of low variance genes to remove. Default None, no filtering.
+        minimum_expression (float): The threshold for minimum expression. Units: mean log2(TPM+1). Default None, no
+            filtering.
     """
     exp_df = pd.concat(expression_dict.values())
 
@@ -24,16 +23,18 @@ def process_expression_compendium(expression_dict, variance_threshold=20, minimu
     numeric_columns = exp_df.select_dtypes(include=[np.number])
 
     # Remove genes with very low expression
-    gene_means = numeric_columns.mean()
-    genes_to_keep = gene_means > minimum_expression
-    filtered_numeric_columns = numeric_columns.loc[:, genes_to_keep]
+    if minimum_expression is not None:
+        gene_means = numeric_columns.mean()
+        genes_to_keep = gene_means > minimum_expression
+        numeric_columns = numeric_columns.loc[:, genes_to_keep]
 
     # Remove genes with the lowest variance
-    gene_variances = filtered_numeric_columns.var()
-    genes_to_keep = gene_variances > np.percentile(gene_variances, variance_threshold)
-    filtered_numeric_columns = filtered_numeric_columns.loc[:, genes_to_keep]
+    if variance_threshold is not None:
+        gene_variances = numeric_columns.var()
+        genes_to_keep = gene_variances > np.percentile(gene_variances, variance_threshold)
+        numeric_columns = numeric_columns.loc[:, genes_to_keep]
 
-    filtered_exp_df = pd.concat([non_numeric_data, filtered_numeric_columns], axis=1)
+    filtered_exp_df = pd.concat([non_numeric_data, numeric_columns], axis=1)
     return filtered_exp_df
 
 
